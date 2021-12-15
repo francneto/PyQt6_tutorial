e74e33a472720057cc02ade701abea2206840c9d
Neto <fc.netoo@gmail.com>
2021-12-15 06:21:01 +0100

    Section1_Lesson5 Codes

--- Section_1/windowthree.py
+++ Section_1/windowthree.py
@@ -5,5 +5,8 @@ app = QApplication(sys.argv)
 
 window = QDialog() #Dont have maximize button feature
 
+# window.statusBar().showMessage("Welcome to PyQt6 Course")## only belongs to MainWindow Application
+# window.menuBar().addMenu("File")## only belongs to MainWindow Application
+
 window.show()
 sys.exit(app.exec())
