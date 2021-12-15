83a670d5e2f5fe31c1713911155ef401fc45ae38
Neto <fc.netoo@gmail.com>
2021-12-15 06:16:54 +0100

    Organizing codes in Folders by Section sequence

--- Section_1/mywindow.py
+++ Section_1/mywindow.py
@@ -2,6 +2,11 @@ from PyQt6.QtWidgets import QApplication, QWidget
 import sys
 
 app = QApplication(sys.argv)
+
 window = QWidget()
+
+# window.statusBar().showMessage("Welcome to PyQt6 Course") ##only belongs to QMainWindow
+
 window.show()
+
 sys.exit(app.exec())
